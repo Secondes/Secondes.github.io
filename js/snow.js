186d0fe5cb771b3ab49054c0fc47b90a5a1df904
xiaowu <moxiaowu495@gmail.com>
2019-12-19 17:53:51 +0800

    Site updated: 2019-12-19 17:53:25

--- js/snow.js
+++ js/snow.js
@@ -4,8 +4,8 @@ function snowFall(snow) {
     /* 可配置属性 */
     snow = snow || {};
     this.maxFlake = snow.maxFlake || 200;   /* 最多片数 */
-    this.flakeSize = snow.flakeSize || 5;  /* 雪花形状 */
-    this.fallSpeed = snow.fallSpeed || 5;   /* 坠落速度 */
+    this.flakeSize = snow.flakeSize || 15;  /* 雪花形状 */
+    this.fallSpeed = snow.fallSpeed || 3;   /* 坠落速度 */
 }
 /* 兼容写法 */
 requestAnimationFrame = window.requestAnimationFrame ||
